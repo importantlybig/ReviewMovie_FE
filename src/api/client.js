0f2e2aa5b2148fda8b1a2a4d7eef49e778a028bd
DaiTrong <wkg6623@autuni.ac.nz>
2022-05-25 16:22:34 +0700

    fix be url again

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const client = axios.create({
-	baseURL: process.env.BE_URL || 'http://localhost:8000/api',
+	// baseURL: process.env.BE_URL || 'http://localhost:8000/api',
+	baseURL: process.env.BE_URL,
 })
 
 export default client
