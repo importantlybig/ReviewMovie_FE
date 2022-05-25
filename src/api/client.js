c13d45b128cc20f56959214be16fc85e141843d1
DaiTrong <wkg6623@autuni.ac.nz>
2022-05-25 16:05:59 +0700

    add be url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const client = axios.create({ baseURL: 'http://localhost:8000/api' })
+const client = axios.create({
+	baseURL: process.env.BE_URL || 'http://localhost:8000/api',
+})
 
 export default client
 
